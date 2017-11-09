237d062b3b8c3a9eb09d972ce2e8b2280baf3bba
mattkobi789 <mattkobi789@gmail.com>
2017-11-09 09:37:25 -0500

    Changed to more efficient and more correct given homework solutions.

---
+++ gettoken.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <cctype>
+#include <string>
+#include "lexer.h"
+using namespace std;
+int lineNumber = 0;
+map<enum TokenType, string> typeName = {
+    { T_INT,     "T_INT" },
+    { T_STRING,  "T_STRING" },
+    { T_SET,     "T_SET" },
+    { T_PRINT,   "T_PRINT" },
+    { T_PRINTLN, "T_PRINTLN" },
+    { T_ID,      "T_ID" },
+    { T_ICONST,  "T_ICONST" },
+    { T_SCONST,  "T_SCONST" },
+    { T_PLUS,    "T_PLUS" },
+    { T_MINUS,   "T_MINUS" },
+    { T_STAR,    "T_STAR" },
+    { T_SLASH,   "T_SLASH" },
+    { T_LPAREN,  "T_LPAREN" },
+    { T_RPAREN,  "T_RPAREN" },
+    { T_SC,      "T_SC" },
+    { T_ERROR,   "T_ERROR" },
+    { T_DONE,    "T_DONE" }
+};
+ostream& operator<<(ostream& o, const Token& t)
+{
+    TokenType tt = t.GetTokenType();
+    switch (tt)
+    {
+    case T_ID:
+    case T_ICONST:
+    case T_SCONST:
+    case T_ERROR:  o << typeName[tt] << "(" << t.GetLexeme() << ")"; break;
+    default:
+        o << typeName[tt]; break;
+    }
+    return o;
+}
+enum State {
+    S_NONE,
+    S_VAR,
+    S_NUM,
+    S_STR,
+    S_COM
+};
+string strip(string s, unsigned int n = 1)
+{
+    int m = s.length() - n;
+    if (m<0) m = 0;
+    return s.substr(0, m);
+}
+Token getToken(istream *is)
+{
+    State s = S_NONE;
+    string lex = "";
+    int c;
+    while ((c = is->get()) != EOF)
+    {
+        lex += c;
+        if (s == S_NONE)
+        {
+            switch (c)
+            {
+            case '+': return Token(T_PLUS, lex);
+            case '-': return Token(T_MINUS, lex);
+            case '*': return Token(T_STAR, lex);
+            case '/': s = S_COM; continue;
+            case '(': return Token(T_LPAREN, lex);
+            case ')': return Token(T_RPAREN, lex);
+            case ';': return Token(T_SC, lex);
+            case '\n': lineNumber++; lex = ""; continue;
+            case '"': s = S_STR; continue;
+            default: break;
+            }
+            if (isalpha(c)) { s = S_VAR; continue; }
+            if (isdigit(c)) { s = S_NUM; continue; }
+            if (isspace(c)) { lex = "";  continue; }
+            return Token(T_ERROR, lex);
+        }
+        else if (s == S_STR)
+        {
+            if (c == '"') return Token(T_SCONST, lex);
+            if (c == '\n') return Token(T_ERROR, lex);
+        }
+        else if (s == S_VAR)
+        {
+            if (isalnum(c)) continue;
+            is->putback(c);
+            lex = strip(lex);
+            if (lex == "int") return Token(T_INT, lex);
+            if (lex == "string") return Token(T_STRING, lex);
+            if (lex == "set") return Token(T_SET, lex);
+            if (lex == "print") return Token(T_PRINT, lex);
+            if (lex == "println") return Token(T_PRINTLN, lex);
+            return Token(T_ID, lex);
+        }
+        else if (s == S_NUM)
+        {
+            if (isdigit(c)) continue;
+            is->putback(c);
+            lex = strip(lex);
+            return Token(T_ICONST, lex);
+        }
+        else if (s == S_COM)
+        {
+            if (lex.length() == 2 && lex[1] != '/')
+            {
+                is->putback(c);
+                lex = strip(lex);
+                return Token(T_SLASH, lex);
+            }
+            if (c == '\n')
+            {
+                s = S_NONE;
+                lex = "";
+            }
+        }
+    }
+    if (s == S_VAR)
+    {
+        if (lex == "int") return Token(T_INT, lex);
+        if (lex == "string") return Token(T_STRING, lex);
+        if (lex == "set") return Token(T_SET, lex);
+        if (lex == "print") return Token(T_PRINT, lex);
+        if (lex == "println") return Token(T_PRINTLN, lex);
+        return Token(T_ID, lex);
+    }
+    else if (s == S_NUM)
+    {
+        return Token(T_ICONST, lex);
+    }
+    else if (s == S_STR)
+    {
+        return Token(T_ERROR, lex);
+    }
+    return Token(T_DONE, "");
+}
--- main.cpp
+++ main.cpp
@@ -1,360 +1,102 @@
 #include <iostream>
-#include <istream>
-#include <sstream>
 #include <fstream>
-#include <string>
-#include <vector>
-#include <algorithm>
 #include <map>
-#include "parser.h"
-
+#include <string>
+#include "lexer.h"
 using namespace std;
-
-/*
-* main.cpp
-*/
-
-int lineNumber;
-
-void error(int linenum, const string& message) {
-    cout << linenum + 1 << ":" << message;
-}
-
-extern Token getToken(istream* br)
+void error(int linenum, const string& message)
 {
-    map<string, TokenType> tknTypMap = {
-        { "int", T_INT },
-        { "string", T_STRING },
-        { "set", T_SET },
-        { "print", T_PRINT },
-        { "println", T_PRINTLN },
-        { "+", T_PLUS },
-        { "-", T_MINUS },
-        { "*", T_STAR },
-        { "/", T_SLASH },
-        { "(", T_LPAREN },
-        { ")", T_RPAREN },
-        { ";", T_SC }
-    };
-    char c;
-    string lexeme = "";
-    bool numFlag = false;
-    bool idFlag = false;
-    bool stringFlag = false;
-    bool firstFlag = true;
-    int lineNumber = 1;
-    while ((c = br->get()))
-    {
-        if (c == '\n')
-        {
-            lineNumber++;
-            if (stringFlag)
-            {
-                lexeme += c;
-                return Token(T_ERROR, lexeme);
-            }
-        }
-        if (firstFlag)
-        {
-            if (isspace(c))
-                continue;
-            switch (c) {
-            case '+':
-                return Token(T_PLUS, "");
-            case '-':
-                return Token(T_MINUS, "");
-            case '*':
-                return Token(T_STAR, "");
-            case '/':
-                return Token(T_SLASH, "");
-            case '(':
-                return Token(T_LPAREN, "");
-            case ')':
-                return Token(T_RPAREN, "");
-            case ';':
-                return Token(T_SC, "");
-            default:
-                break;
-            }
-            if (c == '\"')
-            {
-                stringFlag = true;
-                lexeme += c;
-                firstFlag = false;
-                continue;
-            }
-            if (isalpha(c))
-                idFlag = true;
-            if (isdigit(c))
-                numFlag = true;
-            firstFlag = false;
-        }
-        if (numFlag && isdigit(c))
-            lexeme += c;
-        else if (numFlag)
-        {
-            br->putback(c);
-            return Token(T_ICONST, lexeme);
-        }
-        if (idFlag && isalnum(c))
-            lexeme += c;
-        else if (idFlag)
-        {
-            br->putback(c);
-            if (tknTypMap.count(lexeme) == 1)
-                return Token(tknTypMap[lexeme], "");
-            return Token(T_ID, lexeme);
-        }
-        if (stringFlag && (c == '\"'))
-        {
-            lexeme += c;
-            return Token(T_SCONST, lexeme);
-        }
-        else if (stringFlag)
-        {
-            lexeme += c;
-            continue;
-        }
-        if (br->eof())
-            return Token(T_DONE, "");
-        if (c == '.')
-        {
-            lexeme += c;
-            return Token(T_ERROR, lexeme);
-        }
-        if (!isalnum(c))
-        {
-            lexeme += c;
-            return Token(T_DONE, "");
-        }
-    }
-    return Token(T_DONE, "");
+        cout << linenum + 1 << ":" << message;
 }
-
-extern ostream& operator<<(ostream& out, const Token& tok) {
-    map<TokenType, string> tknTypMap = {
-        { T_INT, "T_INT" },
-        { T_STRING, "T_STRING" },
-        { T_SET, "T_SET" },
-        { T_PRINT, "T_PRINT" },
-        { T_PRINTLN, "T_PRINTLN" },
-        { T_PLUS, "T_PLUS" },
-        { T_MINUS, "T_MINUS" },
-        { T_STAR, "T_STAR" },
-        { T_SLASH, "T_SLASH" },
-        { T_LPAREN, "T_LPAREN" },
-        { T_RPAREN, "T_RPAREN" },
-        { T_SC, "T_SC" },
-        { T_ERROR, "T_ERROR" },
-        { T_ID, "T_ID" },
-        { T_SCONST, "T_SCONST" },
-        { T_ICONST, "T_ICONST" }
-    };
-    if (tok.GetTokenType() == T_ID || tok.GetTokenType() == T_SCONST || tok.GetTokenType() == T_ICONST || tok.GetTokenType() == T_ERROR)
-    {
-        out << tknTypMap[tok.GetTokenType()] << "(" << tok.GetLexeme() << ")";
-        return out;
-    }
-    out << tknTypMap[tok.GetTokenType()];
-    return out;
+bool OPT_V = false, OPT_I = false, OPT_S = false;
+bool endToken(Token& t)
+{
+    if (t.GetTokenType() == T_ERROR) return true;
+    if (t.GetTokenType() == T_DONE) return true;
+    return false;
 }
-
 int main(int argc, char *argv[])
 {
-    ifstream inFile;
-    stringstream inString;
-    string input = "";
-    string buffer = "";
-    bool sFlag = false;
-    bool iFlag = false;
-    bool vFlag = false;
-    bool tFlag = false;
-    bool slashFlag = false;
-    bool firstLoop = true;
-    int fileCounter = 0;
-    int index = -1;
-    int numId = 0;
-    int numSet = 0;
-    int numPlus = 0;
-    int numStar = 0;
-    int lineNumber = 1;
-    Token gotToken;
-    vector<string> strings;
-    vector<string> identifiers;
-    for (int i = 1; i < argc; i++)
+    for (int i = 1; i<argc; i++)
     {
-        if (argv[i][0] == '-')
-        {
-            switch (argv[i][1])
-            {
-            case 's':
-                sFlag = true;
-                break;
-            case 'i':
-                iFlag = true;
-                break;
-            case 'v':
-                vFlag = true;
-                break;
-            case 't':
-                tFlag = true;
-                break;
-            default:
-                cout << argv[i] << " UNRECOGNIZED FLAG" << endl;
-                return 0;
-            }
-        }
+        if (argv[i][0] != '-') continue;
+        string arg(argv[i]);
+        if (arg == "-v") OPT_V = true;
+        else if (arg == "-i") OPT_I = true;
+        else if (arg == "-s") OPT_S = true;
         else
         {
-            fileCounter++;
-            index = i;
+            cout << arg << " INVALID FLAG" << endl;
+            return 1;
         }
     }
-    if (fileCounter > 0)
+    bool foundfile = false;
+    istream *in = &cin;
+    ifstream file;
+    for (int i = 1; i<argc; i++)
     {
-        if (fileCounter > 1)
+        if (argv[i][0] == '-') continue;
+        if (foundfile)
         {
             cout << "TOO MANY FILES" << endl;
-            return 0;
+            return 1;
         }
-        inFile.open(argv[index]);
-        if (!inFile.is_open())
+        foundfile = true;
+        string arg(argv[i]);
+        file.open(arg);
+        if (!file.is_open())
         {
-            cout << argv[index] << " FILE NOT FOUND" << endl;
-            return 0;
+            cout << arg << " FILE NOT FOUND" << endl;
+            return 1;
         }
-        gotToken = getToken(&inFile);
-        while (!(gotToken.GetTokenType() == T_DONE || gotToken.GetTokenType() == T_ERROR))
-        {
-            if (gotToken.GetTokenType() == T_SLASH)
-            {
-                if (slashFlag == true)
-                {
-                    inFile.ignore(256, '\n');
-                    slashFlag = false;
-                }
-                slashFlag = true;
-            }
-            if (slashFlag && gotToken.GetTokenType() != T_SLASH)
-            {
-                slashFlag = false;
-                if (vFlag)
-                    cout << "T_SLASH" << endl;
-            }
-            if (vFlag && gotToken.GetTokenType() != T_SLASH)
-                cout << gotToken << endl;
-            if (gotToken.GetTokenType() == T_ID)
-                numId++;
-            else if (gotToken.GetTokenType() == T_SET)
-                numSet++;
-            else if (gotToken.GetTokenType() == T_PLUS)
-                numPlus++;
-            else if (gotToken.GetTokenType() == T_STAR)
-                numStar++;
-            if (sFlag && gotToken.GetTokenType() == T_SCONST)
-                strings.push_back(gotToken.GetLexeme());
-            if (iFlag && gotToken.GetTokenType() == T_ID)
-                identifiers.push_back(gotToken.GetLexeme());
-            gotToken = getToken(&inFile);
-        }
+        in = &file;
     }
-    else
+    Token t;
+    int tokens = 0, ids = 0, strings = 0;
+    map<string, unsigned int> smap, imap;
+    while (!endToken(t = getToken(in)))
     {
-        while (getline(cin, input))
-        {
-            inString << " " << input;
-        }
-        gotToken = getToken(&inString);
-        while (!(gotToken.GetTokenType() == T_DONE || gotToken.GetTokenType() == T_ERROR))
-        {
-            if (gotToken.GetTokenType() == T_SLASH)
-            {
-                if (slashFlag == true)
-                {
-                    inString.ignore(256, '\n');
-                    slashFlag = false;
-                }
-                slashFlag = true;
-            }
-            if (slashFlag && gotToken.GetTokenType() != T_SLASH)
-            {
-                slashFlag = false;
-                if (vFlag)
-                    cout << "T_SLASH" << endl;
-            }
-            if (vFlag && gotToken.GetTokenType() != T_SLASH)
-                cout << gotToken << endl;
-            if (gotToken.GetTokenType() == T_ID)
-                numId++;
-            else if (gotToken.GetTokenType() == T_SET)
-                numSet++;
-            else if (gotToken.GetTokenType() == T_PLUS)
-                numPlus++;
-            else if (gotToken.GetTokenType() == T_STAR)
-                numStar++;
-            if (sFlag && gotToken.GetTokenType() == T_SCONST)
-                strings.push_back(gotToken.GetLexeme());
-            if (iFlag && gotToken.GetTokenType() == T_ID)
-                identifiers.push_back(gotToken.GetLexeme());
-            gotToken = getToken(&inString);
-        }
+        if (OPT_V) cout << t << endl;
+        tokens++;
+        if (t.GetTokenType() == T_SCONST) { strings++; smap[t.GetLexeme()]++; }
+        if (t.GetTokenType() == T_ID) { ids++; imap[t.GetLexeme()]++; }
     }
-    if (gotToken.GetTokenType() == T_ERROR)
+    if (t.GetTokenType() == T_ERROR)
     {
-        cout << "Lexical error " << gotToken << endl;
-        return 0;
+        cout << "Lexical error " << t << endl;
+        return 1;
     }
-    lineNumber = gotToken.GetLinenum();
-    if (lineNumber < 1)
-        lineNumber++;
-    cout << "Total number of identifiers: " << numId << endl;
-    cout << "Total number of set: " << numSet << endl;
-    cout << "Total number of +: " << numPlus << endl;
-    cout << "Total number of *: " << numStar << endl;
-    if (sFlag)
+    cout << "Token count: " << tokens << endl;
+    cout << "Identifier count: " << ids << endl;
+    cout << "String count: " << strings << endl;
+    if (OPT_S)
     {
-        sort(strings.begin(), strings.end());
-        firstLoop = true;
-        for (vector<string>::iterator sit = strings.begin(); sit != strings.end(); ++sit)
+        map<string, unsigned int>::iterator i;
+        bool first = true;
+        for (i = smap.begin(); i != smap.end(); i++)
         {
-            if (firstLoop)
-            {
-                cout << *sit;
-                firstLoop = false;
-            }
-            else
-            {
-                cout << ", ";
-                cout << *sit;
-            }
+            if (!first) cout << ", ";
+            first = false;
+            cout << i->first;
         }
-        if (!strings.empty())
-            cout << endl;
+        if (!first) cout << endl;
     }
-    if (iFlag)
+    if (OPT_I)
     {
-        sort(identifiers.begin(), identifiers.end());
-        firstLoop = true;
-        for (vector<string>::iterator iit = identifiers.begin(); iit != identifiers.end(); ++iit)
+        map<string, unsigned int>::iterator i;
+        bool first = true;
+        for (i = imap.begin(); i != imap.end(); i++)
         {
-            if (firstLoop)
-            {
-                cout << *iit;
-                firstLoop = false;
-            }
-            else
-            {
-                cout << ", ";
-                cout << *iit;
-            }
+            if (!first) cout << ", ";
+            first = false;
+            cout << i->first;
         }
-        if (!identifiers.empty())
-            cout << endl;
+        if (!first) cout << endl;
     }
     return 0;
     ParseTree *tree = Prog(&cin);
-    if (tree == 0) {
-        // there was some kind of parse error
+    if (tree == 0)
+    {
         return 1;
     }
     return 0;
